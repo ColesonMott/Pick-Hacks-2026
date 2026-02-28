9401792195196c8300ef233e4c756fe15f0d5fd0
Gibson Nichol <gnichol479@LSR7.net>
2026-02-28 14:17:20 -0600

    Revert "Merge branch 'main' of https://github.com/ColesonMott/Pick-Hacks-2026"
    This reverts commit 7c223333e8bff9e6e3e8d51f14c3b3b7f1b4e8da, reversing
    changes made to e996d2aef61b5200f899fead5b724286f6a431ed.

--- Traffic-Light-Simulation/Assets/Scripts/ComplicatedTrafficController.cs
+++ Traffic-Light-Simulation/Assets/Scripts/ComplicatedTrafficController.cs
@@ -51,6 +51,25 @@ public class ComplicatedTrafficController : MonoBehaviour
         StartNormalCycle();
     }
 
+    private void Update()
+    {
+        // Manual testing keys
+        if (Input.GetKeyDown(KeyCode.N))
+            ActivateEmergency("North");
+
+        if (Input.GetKeyDown(KeyCode.S))
+            ActivateEmergency("South");
+
+        if (Input.GetKeyDown(KeyCode.E))
+            ActivateEmergency("East");
+
+        if (Input.GetKeyDown(KeyCode.W))
+            ActivateEmergency("West");
+
+        if (Input.GetKeyDown(KeyCode.R))
+            ResumeNormal();
+    }
+
     #endregion
 
     #region NORMAL CYCLE
